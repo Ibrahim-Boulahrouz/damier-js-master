e71df16a3265c1451ae0ade8385a3fcf26802a0e
b18007615 <ibrahim.boulahrouz@etu.univ-amu.fr>
2020-02-04 16:53:24 +0100

    kdo

--- damier-js-master/js2.js
+++ damier-js-master/js2.js
@@ -1,4 +1,4 @@
-
+/*
 (function() {
 
 	$(document).ready(() => {
@@ -35,5 +35,70 @@
 		}
 		
 	});
+
+}) ();
+
+ */
+
+class Damier {
+	constructor(dest,longueur,hauteur){
+
+		let css_black =  {
+			'display'	   : 'inline-block',
+			'background-color' : 'black',
+			'color'		   : 'white',
+			'min-width'        : '70px',
+			'min-height' 	   : '70px'
+		};
+		let css_white = {
+			'display'	   : 'inline-block',
+			'background-color' : 'white',
+			'color'		   : 'black',
+			'min-width'        : '70px',
+			'min-height' 	   : '70px'
+		};
+
+		let new_css = {
+			'display'	   : 'inline-block',
+			'background-color' : 'blue',
+			'color'		   : 'black',
+			'min-width'        : '70px',
+			'max-width' 	   : '70px'
+		};
+
+		let hover = function() {
+			$(this).css(new_css)
+		};
+
+		let out = function() {
+			$(this).css($(this).data('css_case'))
+		};
+
+
+		for(let y=0;y<longueur;y++) {
+			let ligne = $('<div></div>');
+			$(dest).append(ligne);
+			for(let x=0; x<hauteur;x++) {
+				ligne.append(
+					$('<div></div>')
+						.css((x+y) % 2 ? css_black : css_white)
+						.append("&nbsp;")
+						.data('css_case',(x+y) % 2 ? css_black : css_white)
+						.mouseover(hover)
+						.mouseleave(out)
+
+				);
+			}
+		}
+
+	}
+}
+
+(function() {
+	'use strict' ;
+	$(document).ready(function() {
+	new Damier('#damier', 3, 3);
+	});
+
 
 }) ();
